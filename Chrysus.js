82495872bf8332441e4a3bb1dab5d0f01c48dd1c
Ernest G. Wilson II <ErnestGWilsonII@gmail.com>
2019-11-09 10:46:23 -0500

    Initial commit

--- Chrysus.js
+++ Chrysus.js
@@ -1,5 +1,5 @@
 /**
- * @summary Chrysus Engine - Creates an Chrysus Engine instance based on the type specified
+ * @summary Chrysus Engine - Creates a Chrysus client instance based on the type specified
  * @param {string} type - is required like: "sqs" or "redis"
  * @param {object} params - is required and is based on the type like: { sqsQueue, awsAccessKeyId, awsSecretAccessKey} or { redisHost, redisPort, redisPass, redisChan } or { mqttHost, mmqttPort, mqttPass, mqttChan }
  * @returns {object} res - {}
@@ -9,13 +9,13 @@ class Chrysus {
     if (typeof type == "string") {
       this.type = type;
     } else {
-      throw "CRITICAL: Chrysus Engine type is required and must be a string, like: mqtt, redis or sqs";
+      throw "CRITICAL: Chrysus client type is required and must be a string, like: mqtt, redis or sqs";
     }
 
     if (typeof params == "object") {
       this.params = params;
     } else {
-      throw "CRITICAL: Chrysus Engine parameters are required and are based on the type (mqtt, redis or sqs) and must be an object";
+      throw "CRITICAL: Chrysus client parameters are required and are based on the type (mqtt, redis or sqs) and must be an object";
     }
   }
 
@@ -27,26 +27,26 @@ class Chrysus {
       if (this.type === "mqtt") {
         const { redisSubscriber } = require("./lib/mqttSubscriber");
         redisSubscriber(this.params);
-        console.log("Chrysus Engine type: " + res.type);
+        console.log("Chrysus client type: " + res.type);
         resolve(res);
       }
       // Redis
       if (this.type === "redis") {
         const { redisSubscriber } = require("./lib/redisSubscriber");
         redisSubscriber(this.params);
-        console.log("Chrysus Engine type: " + res.type);
+        console.log("Chrysus client type: " + res.type);
         resolve(res);
       }
       // SQS
       if (this.type === "sqs") {
         const { sqsSubscriber } = require("./lib/sqsSubscriber");
         sqsSubscriber(this.params);
-        console.log("Chrysus Engine type: " + res.type);
+        console.log("Chrysus client type: " + res.type);
         resolve(res);
       }
       // Unknown
       reject(
-        "CRITICAL: Chrysus Engine type: unknown! Try using one of: mqtt, redis or sqs"
+        "CRITICAL: Chrysus client type: unknown! Try using one of: mqtt, redis or sqs"
       );
     });
   }
