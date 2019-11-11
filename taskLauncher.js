24e5a3213e32d9d0cb2943d296f4817c6d9b064b
Ernest G. Wilson II <ErnestGWilsonII@gmail.com>
2019-11-11 05:50:11 -0500

    Initial commit

--- taskLauncher.js
+++ taskLauncher.js
@@ -1,7 +1,3 @@
-// https://timonweb.com/tutorials/how-to-enable-ecmascript-6-imports-in-nodejs/
-// node -r esm taskLauncher.js
-// node --experimental-modules taskLauncher.js
-
 /**
  * @summary Blah
  * @param {string} target - is required and
@@ -10,15 +6,15 @@
 async function taskLauncher(target, taskName) {
   try {
     if (target) {
-      taskPath = "./" + taskName + ".mjs";
+      taskPath = "./" + taskName;
 
       console.log("taskLauncher - taskPath: " + taskPath);
       // console.log("taskLauncher - target: " + target);
       console.log("taskLauncher - taskName: " + taskName);
 
-      let module = await import(taskPath);
-      let res = await module.task(target);
-      console.log("output: " + JSON.stringify(res));
+      const { task } = await require(taskPath);
+      let res = await task(target);
+      // console.log("output: " + JSON.stringify(res));
       return res;
     }
   } catch (error) {
@@ -31,7 +27,7 @@ async function launchIt(target, taskName) {
     if (target) {
       //console.log("launchIt - taskName: " + taskName + " target: " + target);
       let res = await taskLauncher(target, taskName);
-      //console.log(res);
+      console.log("output: " + JSON.stringify(res));
     }
   } catch (error) {
     console.error(error);
@@ -44,5 +40,8 @@ for (taskName of taskTypesArray) {
   let target = "127.0.0.1";
   launchIt(target, taskName);
 }
+
+// let target = "127.0.0.1";
+// taskLauncher(target);
 
 module.exports.taskLauncher = taskLauncher;
--- task_linux_ping.js
+++ task_linux_ping.js
@@ -3,13 +3,14 @@
  * @param {string} target - is required and
  * @returns {object} res - returns the
  * */
-export async function task(target) {
+async function task(target) {
   try {
     if (target) {
-      let res = "Target responded to trace: " + target;
+      let res = "Target responded to ping: " + target;
       return res;
     }
   } catch (error) {
     console.error(error);
   }
 }
+module.exports.task = task;
