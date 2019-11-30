c57418eb9825829cbdde20f28e2f006b68990fc1
Ernest G. Wilson II <ErnestGWilsonII@gmail.com>
2019-11-30 06:41:03 -0500

    Initial commit

--- redisExample.js
+++ redisExample.js
@@ -1,8 +1,10 @@
-//@ts-check
 // Example: Launching a Chrysus Engine using Redis pub/sub
 const { Chrysus } = require("./Chrysus");
 
+// Choose a valid Chrysus type
 let type = "redis";
+
+// Define the required parameters based on the specific type
 let params = {
   redisHost: process.env.CHRYSUS_REDISHOST,
   redisPort: process.env.CHRYSUS_REDISPORT,
@@ -10,5 +12,8 @@ let params = {
   redisChan: process.env.CHRYSUS_REDISCHAN
 };
 
-const chrysus = new Chrysus(type, params);
+// Create an instance of Chrysus
+const chrysus = new Chrysus(type, params); //@ts-check
+
+// Launch the Chrysus Engine
 chrysus.engine();
--- sqsExample.js
+++ sqsExample.js
@@ -1,13 +1,18 @@
-//@ts-check
 // Example: Launching a Chrysus Engine using SQS pub/sub
 const { Chrysus } = require("./Chrysus");
 
+// Choose a valid Chrysus type
 let type = "sqs";
+
+// Define the required parameters based on the specific type
 let params = {
   sqsQueue: process.env.SQS_QUEUE,
   awsAccessKeyId: process.env.SQS_QUEUE,
   awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 };
 
-const chrysus = new Chrysus(type, params);
+// Create an instance of Chrysus
+const chrysus = new Chrysus(type, params); //@ts-check
+
+// Launch the Chrysus Engine
 chrysus.engine();
